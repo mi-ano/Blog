d40c96fa517ec44b99f8efbe12336c91014ee9c6
mi-ano <ryanmiano68@gmail.com>
2019-08-13 09:20:49 +0300

    added text re indent

--- app/models.py
+++ app/models.py
@@ -9,103 +9,94 @@ from datetime import datetime
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-    class User(UserMixin, db.Model):
-        __tablename__ = 'users'
 
-        id = db.Column(db.Integer, primary_key=True)
-        username = db.Column(db.String(255), index=True)
-        email = db.Column(db.String(255), unique=True, index=True)
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
 
-        bio = db.Column(db.String(255))
-        profile_pic_path = db.Column(db.String())
-        password_secure = db.Column(db.String(255))
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), index=True)
+    email = db.Column(db.String(255), unique=True, index=True)
 
-        @property
-        def password(self):
-            raise AttributeError("You cannot read the password attribute")
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_secure = db.Column(db.String(255))
 
-            @password.setter
-            def password(self, password):
-                self.password_hash = generate_password_hash(password)
+    @property
+    def password(self):
+        raise AttributeError("You cannot read the password attribute")
 
-                def verify_password(self, password):
-                    return check_password_hash(self.password_hash, password)
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
 
-                    def save_user(self):
-                        db.session.add(self)
-                        db.session.commit()
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
-                        def __repr__(self):
-                            return f'User{self.username}'
+    def save_user(self):
+        db.session.add(self)
+        db.session.commit()
 
-                            class Subscriber(db.Model):
-                                __tablename__ = 'subscribers'
+    def __repr__(self):
+        return f'User{self.username}'
 
-                                id = db.Column(db.Integer, primary_key=True)
-                                email = db.Column(
-                                    db.String(255), unique=True, index=True)
 
-                                def save_subscriber(self):
-                                    db.session.add(self)
-                                    db.session.commit()
+class Subscriber(db.Model):
+    __tablename__ = 'subscribers'
 
-                                    class Post(db.Model):
-                                        __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True, index=True)
 
-                                        id = db.Column(
-                                            db.Integer, primary_key=True)
-                                        post_id = db.Column(db.Integer)
-                                        title = db.Column(db.String)
-                                        post = db.Column(db.String)
-                                        category = db.Column(db.String)
-                                        like = db.Column(db.Integer)
-                                        posted = db.Column(
-                                            db.DateTime, default=datetime.utcnow)
+    def save_subscriber(self):
+        db.session.add(self)
+        db.session.commit()
 
-                                        def save_post(self):
-                                            db.session.add(self)
-                                            db.session.commit()
 
-                                            @classmethod
-                                            def get_posts(cls, id):
-                                                posts = Post.query.filter_by(
-                                                    post_id=id).all()
-                                                return posts
+class Post(db.Model):
+    __tablename__ = 'posts'
 
-                                                class Comment(db.Model):
-                                                    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer)
+    title = db.Column(db.String)
+    post = db.Column(db.String)
+    category = db.Column(db.String)
+    like = db.Column(db.Integer)
+    posted = db.Column(db.DateTime, default=datetime.utcnow)
 
-                                                    id = db.Column(
-                                                        db.Integer, primary_key=True)
-                                                    comment = db.Column(
-                                                        db.String)
-                                                    posted = db.Column(
-                                                        db.DateTime, default=datetime.utcnow)
-                                                    post_id = db.Column(
-                                                        db.Integer, db.ForeignKey("posts.id"))
-                                                    user_id = db.Column(
-                                                        db.Integer, db.ForeignKey("users.id"))
+    def save_post(self):
+        db.session.add(self)
+        db.session.commit()
 
-                                                    def save_comment(self):
-                                                        db.session.add(self)
-                                                        db.session.commit()
+    @classmethod
+    def get_posts(cls, id):
+        posts = Post.query.filter_by(post_id=id).all()
+        return posts
 
-                                                        def delete_comment(self):
-                                                            db.session.delete(
-                                                                self)
-                                                            db.session.commit()
 
-                                                            class Role(db.Model):
-                                                                __tablename__ = 'roles'
+class Comment(db.Model):
+    __tablename__ = 'comments'
 
-                                                                id = db.Column(
-                                                                    db.Integer, primary_key=True)
-                                                                name = db.Column(
-                                                                    db.String(255))
-                                                                users = db.relationship(
-                                                                    'User', backref='role', lazy="dynamic")
-                                                                role_id = db.Column(
-                                                                    db.Integer, db.ForeignKey('roles.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+    posted = db.Column(db.DateTime, default=datetime.utcnow)
+    post_id = db.Column(db.Integer, db.ForeignKey("posts.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
 
-                                                                def __repr__(self):
-                                                                    return f'User{self.name}'
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_comment(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+class Role(db.Model):
+     __tablename__ = 'roles'
+     
+     id = db.Column(db.Integer, primary_key=True)
+     name = db.Column(db.String(255))
+     users= db.relationship('User',backref='role',lazy="dynamic")
+     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+                                                                            
+    def __repr__(self):
+       return f'User{self.name}
